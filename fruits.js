f592b2ee764391c5a55d27b38301baad07d2d5d5
nyakky7 <158140590+nyakky7@users.noreply.github.com>
2024-01-30 17:04:45 +0900

    Add files via upload

--- fruits.js
+++ fruits.js
@@ -1,57 +1,95 @@
-export const FRUITS = [
+const FRUITS_BASE = [
   {
-    label: "cherry",
-    radius: 40 / 2,
-    color: "#F20306",
+    name: "base/00_cherry",
+    radius: 33 / 2,
   },
   {
-    label: "strawberry",
-    radius: 50 / 2,
-    color: "#FF624C",
+    name: "base/01_strawberry",
+    radius: 48 / 2,
   },
   {
-    label: "grape",
-    radius: 72 / 2,
-    color: "#A969FF",
+    name: "base/02_grape",
+    radius: 61 / 2,
   },
   {
-    label: "kumkwat",
-    radius: 85 / 2,
-    color: "#FFAF02",
+    name: "base/03_gyool",
+    radius: 69 / 2,
   },
   {
-    label: "orange",
-    radius: 106 / 2,
-    color: "#FC8611",
+    name: "base/04_orange",
+    radius: 89 / 2,
   },
   {
-    label: "apple",
-    radius: 140 / 2,
-    color: "#F41615",
+    name: "base/05_apple",
+    radius: 114 / 2,
   },
   {
-    label: "pear",
-    radius: 160 / 2,
-    color: "#FDF176",
+    name: "base/06_pear",
+    radius: 129 / 2,
   },
   {
-    label: "peach",
-    radius: 196 / 2,
-    color: "#FEB6AC",
+    name: "base/07_peach",
+    radius: 156 / 2,
   },
   {
-    label: "pineapple",
+    name: "base/08_pineapple",
+    radius: 177 / 2,
+  },
+  {
+    name: "base/09_melon",
     radius: 220 / 2,
-    color: "#F7E608",
   },
   {
-    label: "melon",
-    radius: 270 / 2,
-    color: "#89CE13",
-  },
-  {
-    label: "watermelon",
-    radius: 300 / 2,
-    color: "#26AA1E",
+    name: "base/10_watermelon",
+    radius: 259 / 2,
   },
 ];
+
+const FRUITS_HLW = [
+  {
+    name: "halloween/00_cherry",
+    radius: 46 / 2,
+  },
+  {
+    name: "halloween/01_strawberry",
+    radius: 55 / 2,
+  },
+  {
+    name: "halloween/02_grape",
+    radius: 67 / 2,
+  },
+  {
+    name: "halloween/03_gyool",
+    radius: 81 / 2,
+  },
+  {
+    name: "halloween/04_orange",
+    radius: 97 / 2,
+  },
+  {
+    name: "halloween/05_apple",
+    radius: 117 / 2,
+  },
+  {
+    name: "halloween/06_pear",
+    radius: 142 / 2,
+  },
+  {
+    name: "halloween/07_peach",
+    radius: 171 / 2,
+  },
+  {
+    name: "halloween/08_pineapple",
+    radius: 206 / 2,
+  },
+  {
+    name: "halloween/09_melon",
+    radius: 249 / 2,
+  },
+  {
+    name: "halloween/10_watermelon",
+    radius: 300 / 2,
+  },
+];
+
+export { FRUITS_BASE, FRUITS_HLW };
--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-import {
-  Engine,
-  Render,
-  Runner,
-  Bodies,
-  World,
-  Body,
-  Sleeping,
-  Events,
-} from "matter-js";
-import { FRUITS } from "./fruits";
+import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
+import { FRUITS_BASE, FRUITS_HLW } from "./fruits";
+import "./dark.css";
+
+let THEME = "halloween"; // { base, halloween }
+let FRUITS = FRUITS_BASE;
+
+switch (THEME) {
+  case "halloween":
+    FRUITS = FRUITS_HLW;
+    break;
+  default:
+    FRUITS = FRUITS_BASE;
+}
 
 const engine = Engine.create();
 const render = Render.create({
@@ -19,153 +22,149 @@ const render = Render.create({
     background: "#F7F4C8",
     width: 620,
     height: 850,
-  },
+  }
 });
 
 const world = engine.world;
 
-const ground = Bodies.rectangle(310, 820, 620, 60, {
-  isStatic: true,
-  render: {
-    fillStyle: "#E6B143",
-  },
-});
 const leftWall = Bodies.rectangle(15, 395, 30, 790, {
   isStatic: true,
-  render: {
-    fillStyle: "#E6B143",
-  },
-});
-const rightWall = Bodies.rectangle(605, 395, 30, 790, {
-  isStatic: true,
-  render: {
-    fillStyle: "#E6B143",
-  },
-});
-const topLine = Bodies.rectangle(310, 150, 620, 2, {
-  isStatic: true,
-  isSensor: true,
-  render: { fillStyle: "#E6B143" },
-  label: "topLine",
+  render: { fillStyle: "#E6B143" }
 });
 
-World.add(world, [ground, leftWall, rightWall, topLine]);
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
+});
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
+});
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
+  name: "topLine",
+  isStatic: true,
+  isSensor: true,
+  render: { fillStyle: "#E6B143" }
+})
+
+World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
 Runner.run(engine);
 
 let currentBody = null;
 let currentFruit = null;
-let interval = null;
 let disableAction = false;
+let interval = null;
 
-function addCurrentFruit() {
-  const randomFruit = getRandomFruit();
+function addFruit() {
+  const index = Math.floor(Math.random() * 5);
+  const fruit = FRUITS[index];
 
-  const body = Bodies.circle(300, 50, randomFruit.radius, {
-    label: randomFruit.label,
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
     isSleeping: true,
     render: {
-      fillStyle: randomFruit.color,
-      sprite: { texture: `/${randomFruit.label}.png` },
+      sprite: { texture: `${fruit.name}.png` }
     },
     restitution: 0.2,
   });
 
   currentBody = body;
-  currentFruit = randomFruit;
+  currentFruit = fruit;
 
   World.add(world, body);
 }
 
-function getRandomFruit() {
-  const randomIndex = Math.floor(Math.random() * 5);
-  const fruit = FRUITS[randomIndex];
-
-  if (currentFruit && currentFruit.label === fruit.label)
-    return getRandomFruit();
-
-  return fruit;
-}
-
 window.onkeydown = (event) => {
-  if (disableAction) return;
+  if (disableAction) {
+    return;
+  }
 
   switch (event.code) {
-    case "ArrowLeft":
-      if (interval) return;
+    case "KeyA":
+      if (interval)
+        return;
+
       interval = setInterval(() => {
-        if (currentBody.position.x - 20 > 30)
+        if (currentBody.position.x - currentFruit.radius > 30)
           Body.setPosition(currentBody, {
             x: currentBody.position.x - 1,
             y: currentBody.position.y,
           });
       }, 5);
       break;
-    case "ArrowRight":
-      if (interval) return;
+
+    case "KeyD":
+      if (interval)
+        return;
+
       interval = setInterval(() => {
-        if (currentBody.position.x + 20 < 590)
-          Body.setPosition(currentBody, {
-            x: currentBody.position.x + 1,
-            y: currentBody.position.y,
-          });
+        if (currentBody.position.x + currentFruit.radius < 590)
+        Body.setPosition(currentBody, {
+          x: currentBody.position.x + 1,
+          y: currentBody.position.y,
+        });
       }, 5);
       break;
-    case "Space":
+
+    case "KeyS":
+      currentBody.isSleeping = false;
       disableAction = true;
-      Sleeping.set(currentBody, false);
+
       setTimeout(() => {
-        addCurrentFruit();
+        addFruit();
         disableAction = false;
       }, 1000);
+      break;
   }
-};
+}
 
 window.onkeyup = (event) => {
   switch (event.code) {
-    case "ArrowLeft":
-    case "ArrowRight":
+    case "KeyA":
+    case "KeyD":
       clearInterval(interval);
       interval = null;
   }
-};
+}
 
 Events.on(engine, "collisionStart", (event) => {
   event.pairs.forEach((collision) => {
-    if (collision.bodyA.label === collision.bodyB.label) {
+    if (collision.bodyA.index === collision.bodyB.index) {
+      const index = collision.bodyA.index;
+
+      if (index === FRUITS.length - 1) {
+        return;
+      }
+
       World.remove(world, [collision.bodyA, collision.bodyB]);
 
-      const index = FRUITS.findIndex(
-        (fruit) => fruit.label === collision.bodyA.label
-      );
-
-      // If last fruit, do nothing
-      if (index === FRUITS.length - 1) return;
-
       const newFruit = FRUITS[index + 1];
-      const body = Bodies.circle(
+
+      const newBody = Bodies.circle(
         collision.collision.supports[0].x,
         collision.collision.supports[0].y,
         newFruit.radius,
         {
           render: {
-            fillStyle: newFruit.color,
-            sprite: { texture: `/${newFruit.label}.png` },
+            sprite: { texture: `${newFruit.name}.png` }
           },
-          label: newFruit.label,
+          index: index + 1,
         }
       );
-      World.add(world, body);
+
+      World.add(world, newBody);
     }
+
     if (
-      (collision.bodyA.label === "topLine" ||
-        collision.bodyB.label === "topLine") &&
-      !disableAction
-    ) {
+      !disableAction &&
+      (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
       alert("Game over");
     }
   });
 });
 
-addCurrentFruit();
+addFruit();
